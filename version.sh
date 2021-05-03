d885f4120ce3309b80ab1e68d5be26e66f8e6b04
AvaPL <241574@student.pwr.edu.pl>
2021-05-03 16:15:29 +0200

    Changed version detection method, slightly improved output logs.

--- version.sh
+++
@@ -1,8 +0,0 @@
-version=$(grep "version" "$1")
-regex="version\s*=\s*([0-9\.]+)"
-if [[ $version =~ $regex ]]
-then
-  echo "${BASH_REMATCH[1]}"
-else
-  echo -e "\033[0;31mERROR: unable to find version in $1" >&2
-fi
